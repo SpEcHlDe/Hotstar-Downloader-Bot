566b519373be7efe2fd62e811f0ebf5f36ab0ec6
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-09 03:58:58 -0600

    Initial commit

--- database/adduser.py
+++ database/adduser.py
@@ -4,5 +4,5 @@ from pyrogram.types import Message
 
 
 async def AddUser(bot: Client, update: Message):
-    if not await clinton.is_user_exist(update.from_user.id):
-           await clinton.add_user(update.from_user.id)
+    if not await db.is_user_exist(update.from_user.id):
+           await db.add_user(update.from_user.id)
