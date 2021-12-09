11e5b4413a57535e2ac0e00f350f8f07c6932548
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-09 04:02:13 -0600

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -24,6 +24,7 @@ from pyrogram import Client, filters
 
 from translation import Translation
 from helper_funcs.database import *
+from database.access import db
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -127,7 +128,7 @@ async def show_thumb(bot, update):
 
 async def Gthumb01(bot, update):
     thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await Client.get_thumbnail(update.from_user.id)
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
     if db_thumbnail is not None:
         thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
         Image.open(thumbnail).convert("RGB").save(thumbnail)
@@ -141,7 +142,7 @@ async def Gthumb01(bot, update):
 
 async def Gthumb02(bot, update, duration, download_directory):
     thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await Client.get_thumbnail(update.from_user.id)
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
     if db_thumbnail is not None:
         thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
     else:
