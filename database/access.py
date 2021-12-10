b1e3d0c6f5f25a97accf9c007d8ef05807ccda16
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-10 16:20:51 -0600

    Update access.py

--- database/access.py
+++ database/access.py
@@ -2,4 +2,4 @@
 from sample_config import Config
 from database.database import Database
 
-db = Database(Config.DATABASE_URL, Config.SESSION_NAME)
+clinton = Database(Config.DATABASE_URL, Config.SESSION_NAME)
